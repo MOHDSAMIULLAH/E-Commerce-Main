404d6d9ec3f81df555e0f35dabfb3f8297c35e8c
Samiullah <samiullah0813@gmail.com>
2023-05-02 14:03:29 +0530

    title updated

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>E-Commerce App</title>
       </Head>
       <header>
         <Navbar />
